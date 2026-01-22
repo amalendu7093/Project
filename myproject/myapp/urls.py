947bb2209374cb9a60196d0be29e4321e2cb506d
amalendu7093 <amalendusyamlal@gmail.com>
2026-01-22 15:59:22 +0530

    dipaly home page

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path("home",views.home,name="home"),
+    path("",views.home,name="home"),
     path("html2",views.html2,name="html2")
 
    
